533489ccc890d93a39de9e7fcae237fdfa833307
Ajnavi Kumar <ajnavi@gmail.com>
2016-09-27 22:51:02 -0700

    Process various Wait* annotations.

--- src/main/java/com/qa/selenium/ui/AbstractPage.java
+++ src/main/java/com/qa/selenium/ui/AbstractPage.java
@@ -8,6 +8,8 @@ public abstract class AbstractPage extends LoadableComponent <AbstractPage> {
     public AbstractPage() {
         PageFactory.initElements (Browser.getDriver (), this);
         this.get ();
+        // Process various Wait* annotations at the page, and at WebElement levels
+        Browser.waitFor(this);
     }
 
     @Override
